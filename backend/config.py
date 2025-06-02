cb086e5c2f907e682c4a93d29bb1b1c015d1e57e
FranciscoGinseg <158171560+FranRodriguezVidal@users.noreply.github.com>
2025-06-02 23:33:10 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     UPLOAD_FOLDER = 'uploads/'
-    DB_HOST = 'localhost'
-    DB_USER = 'root'
-    DB_PASSWORD = ''
+    DB_HOST = 'db4free.net'
+    DB_USER = 'buysuser'
+    DB_PASSWORD = 'BuysTickets2005'
     DB_NAME = 'buystickets'
