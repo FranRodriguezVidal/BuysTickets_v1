f522246104fd16765f2297d4cbc540cdf746b094
FranciscoGinseg <158171560+FranRodriguezVidal@users.noreply.github.com>
2025-06-03 12:33:52 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     UPLOAD_FOLDER = 'uploads/'
-    DB_HOST = 'db4free.net'
-    DB_USER = 'buysuser'
-    DB_PASSWORD = 'BuysTickets2005'
-    DB_NAME = 'buystickets'
+    DB_HOST = 'bwxfvn0gpk0gvlgjnipg-mysql.services.clever-cloud.com'
+    DB_USER = 'uxjnjmvaffwj0zfo'
+    DB_PASSWORD = '0tADdrBYJVAPW89tJfYI'
+    DB_NAME = 'bwxfvn0gpk0gvlgjnipg'
