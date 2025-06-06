1ab97eb2fb9827e9ed188265f8a67bdd8f22b2f6
kekielbus <kevinkielbus@gmail.com>
2025-06-06 21:43:58 +0200

    cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,7 +13,7 @@ from flask import send_from_directory
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # ← Esto es importante
+    CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
