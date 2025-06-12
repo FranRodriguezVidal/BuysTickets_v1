7dd30e0141d5775da4142dda0536eeaba7f052f4
kekielbus <kevinkielbus@gmail.com>
2025-06-12 13:56:08 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ class Config:
     UPLOAD_FOLDER = 'uploads/'
     DB_HOST = os.getenv('DB_HOST', 'turntable.proxy.rlwy.net')
     DB_USER = os.getenv('DB_USER', 'root')
-    DB_PORT = os.getenv('DB_PORT', '20986')
+    DB_PORT = os.getenv('DB_PORT', '3306')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'bFEzIjMzYzTCrrmgdWWdOkFowAQxLyGE')
     DB_NAME = os.getenv('DB_NAME', 'railway')
